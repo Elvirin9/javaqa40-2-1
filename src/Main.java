8adca1acff79742c161b71f61a3b0e73abacbad3
Elvirin9 <kaa-lin@yandex.ru>
2022-11-16 00:38:01 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 50000;
+        int milesperPrice = 1;
+        int miles = 0;
+
+        milesperPrice = price / 20;
+        miles =  milesperPrice;
+
+        System.out.println((miles) + " миль ");
+    }
+}
